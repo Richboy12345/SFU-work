0f0f8eb10c91c5821e62edceec5613342e4b9d52
Richboy12345 <33295060+Richboy12345@users.noreply.github.com>
2021-11-22 17:29:02 -0800

    Add files via upload

--- cmpt213/assn1/src/Main.java
+++ cmpt213/assn1/src/Main.java
@@ -6,125 +6,13 @@
  *
  */
 
-import java.util.*;
-import java.lang.*;
-
+//main class, initiates a game
 public class Main {
-
-     //global variable declarations
-     Scanner scan = new Scanner(System.in);
-     ArrayList<Tokimon> tokimons = new ArrayList<Tokimon>();
-
-     public void displayMainMenu() {
-          System.out.print("*************\n * Main Menu *\n *************\n 1. List Tokimons\n 2. Add a new Tokimon\n 3. Remove a Tokimon\n 4. Change Tokimon strength\n 5. DEBUG: Dump objects toString\n 6. Exit\n Which operation should be performed? ");
-          int choice = scan.nextInt();
-          while (choice < 1 || 6 < choice) {
-               System.out.print("User entry was invalid, please try again: ");
-               choice = scan.nextInt();
-          }
-          scan.nextLine();
-          switch(choice) {
-               case 1:
-                    displayAllTokis();
-                    break;
-               case 2:
-                    addNewToki();
-                    break;
-               case 3:
-                    deleteToki();
-                    break;
-               case 4:
-                    alterToki();
-                    break;
-               case 5:
-                    debug();
-                    break;
-               case 6:
-                    return;
-          }
-     }
-
-     public void displayAllTokis() {
-          System.out.println("*********************\n * List of Tokimons: *\n *********************");
-          for (int i = 0; i < tokimons.size(); i++) {
-               System.out.println((i + 1) + ". " + tokimons.get(i).getName() + "; Type: " + tokimons.get(i).getType() + ", Size: " + tokimons.get(i).getSize() + "m, Strength: " + tokimons.get(i).getStrength() + ".");
-          }
-          System.out.print("Press enter to return to the main menu...");
-          scan.nextLine();
-          displayMainMenu();
-     }
-
-     public void addNewToki() {
-          System.out.println("Adding new Tokimon to the database.");
-          System.out.print("Enter its name: ");
-          String name = scan.nextLine();
-          System.out.print("Enter its type: ");
-          String type = scan.nextLine();
-          System.out.print("Enter its size: ");
-          double size = scan.nextDouble();
-          scan.nextLine();
-          Tokimon toki = new Tokimon(name, type, size);
-          tokimons.add(toki);
-          displayMainMenu();
-     }
-
-     public void deleteToki() {
-          displayAllTokis();
-          System.out.print("Which entry should be removed? Enter 0 to cancel: ");
-          int choice = scan.nextInt();
-          while (choice < 0 || tokimons.size() < choice) {
-               System.out.print("User entry was invalid, please try again: ");
-               choice = scan.nextInt();
-          }
-          scan.nextLine();
-          if (choice == 0) {
-               System.out.println("Operation cancelled, returning to menu.");
-          }
-          else {
-               String name = tokimons.get(choice - 1).getName();
-               tokimons.remove(choice - 1);
-               System.out.println(name + "was removed from the list, returning to menu.");
-          }
-          displayMainMenu();
-     }
-
-     public void alterToki() {
-          displayAllTokis();
-          System.out.print("Which entry should be changed? Enter 0 to cancel: ");
-          int choice = scan.nextInt();
-          while (choice < 0 || tokimons.size() < choice) {
-               System.out.print("User entry was invalid, please try again: ");
-               choice = scan.nextInt();
-          }
-          scan.nextLine();
-          if (choice == 0) {
-               System.out.println("Operation cancelled, returning to menu.");
-          }
-          else {
-               String name = tokimons.get(choice - 1).getName();
-               int strength = tokimons.get(choice - 1).getStrength();
-               System.out.print(name + " currently has a strength of " + strength + ". By how much should it change? ");
-               int change = scan.nextInt();
-               change += strength;
-               while (change < 0 || change > 100) {
-                    System.out.print("User entry was invalid, please try again: ");
-                    change = scan.nextInt();
-                    change += strength;
-               }
-               scan.nextLine();
-               tokimons.get(choice - 1).setStrength(change);
-               System.out.println(name + " had their strength changed from " + strength + " to " + change + ", returning to menu.");
-          }
-          displayMainMenu();
-     }
-
-     public void debug() {
-          for (int i = 0; i < tokimons.size(); i++) {
-               System.out.println(tokimons.get(i).toString());
-          }
-     }
-     public static void main(String[] args) {
-          System.out.println("***********************************************\n * Tokimon Tracker by Richard Zhang, 301402349 *\n ***********************************************");
-          displayMainMenu();
-     }
+    public static void main(String[] args) {
+        System.out.println("***********************************************\n" +
+                "* Tokimon Tracker by Richard Zhang, 301402349 *\n" +
+                "***********************************************");
+        Game newGame = new Game();
+        newGame.displayMainMenu();
+    }
 }
--- cmpt213/assn1/src/Tokimon.java
+++ cmpt213/assn1/src/Tokimon.java
@@ -6,47 +6,51 @@
 
 public class Tokimon {
 
-     //Tokimon attributes variable declarations
-     private String name;
-     private String type;
-     private double size;
-     private int strength;
+    //Tokimon attributes variable declarations
+    private String name;
+    private String type;
+    private double size;
+    private int strength;
 
-     //Tokimon object constructor
-     Tokimon(String name, String type, double size){
-          this.name = name;
-          this.type = type;
-          this.size = size;
-          this.strength = 0;
-     }
+    //Tokimon object constructor
+    Tokimon(String name, String type, double size){
+        this.name = name;
+        this.type = type;
+        this.size = size;
+        this.strength = 0;
+    }
 
-     //Tokimon to string override for debug purposes
-     public String toString() {
-          return "Tokimon{" + this.name + "; Type: " + this.type + ", Size: " + this.size + ", Strength: " + this.strength + "}";
-     }
+    //Tokimon to string override for debug purposes
+    public String toString() {
+        return "Tokimon{ Name: " + this.name +
+                "; Type: " + this.type +
+                ", Size: " + this.size +
+                ", Strength: " + this.strength + "}";
+    }
 
-     public String getName() {
-          return this.name;
-     }
-     public String getType() {
-          return this.type;
-     }
-     public double getSize() {
-          return this.size;
-     }
-     public int getStrength() {
-          return this.strength;
-     }
-     public void setName(String name) {
-          this.name = name;
-     }
-     public void setType(String type) {
-          this.type = type;
-     }
-     public void setSize(double size) {
-          this.size = size;
-     }
-     public void setStrength(int strength) {
-          this.strength = strength;
-     }
+    //getters and setters for all attributes
+    public String getName() {
+        return this.name;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public double getSize() {
+        return this.size;
+    }
+    public int getStrength() {
+        return this.strength;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setSize(double size) {
+        this.size = size;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
